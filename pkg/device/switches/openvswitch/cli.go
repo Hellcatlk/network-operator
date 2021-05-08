300507cb4448d727c318405414fe94d2085432a3
Zou Yu <zouy.fnst@cn.fujitsu.com>
2021-05-08 02:13:25 -0400

    Avoid gosec warnings
    Signed-off-by: Zou Yu <zouy.fnst@cn.fujitsu.com>

--- pkg/device/switches/openvswitch/cli.go
+++ pkg/device/switches/openvswitch/cli.go
@@ -37,7 +37,7 @@ type CLI struct {
 
 // PowerOn enable openvswitch
 func (c *CLI) PowerOn(ctx context.Context) (err error) {
-	return ssh.Run(c.address, c.username, c.password, exec.Command("ovs-vsctl", "list", "br", c.bridge))
+	return ssh.Run(c.address, c.username, c.password, exec.Command("ovs-vsctl", "list", "br", c.bridge)) // #nosec
 }
 
 // PowerOff disable openvswitch
@@ -65,10 +65,10 @@ func (c *CLI) SetPortAttr(ctx context.Context, portID string, configuration *v1a
 		}
 	}
 
-	return ssh.Run(c.address, c.username, c.password, exec.Command("ovs-vsctl", "set ", "port", portID, tag))
+	return ssh.Run(c.address, c.username, c.password, exec.Command("ovs-vsctl", "set ", "port", portID, tag)) // #nosec
 }
 
 // ResetPort remove all configure of the port
 func (c *CLI) ResetPort(ctx context.Context, portID string, configuration *v1alpha1.SwitchPortConfiguration) (err error) {
-	return ssh.Run(c.address, c.username, c.password, exec.Command("ovs-vsctl", "set ", "port", portID, "tag="))
+	return ssh.Run(c.address, c.username, c.password, exec.Command("ovs-vsctl", "set ", "port", portID, "tag=")) // #nosec
 }
