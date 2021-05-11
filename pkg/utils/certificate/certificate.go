e44e6d1ce947d93ba2b77b19ea2a47b3f2c8fbea
Zou Yu <zouy.fnst@cn.fujitsu.com>
2021-05-11 05:45:25 -0400

    Update certificate package
    Signed-off-by: Zou Yu <zouy.fnst@cn.fujitsu.com>

--- pkg/utils/certificate/certificate.go
+++ pkg/utils/certificate/certificate.go
@@ -2,7 +2,6 @@ package certificate
 
 import (
 	"context"
-	"encoding/base64"
 	"fmt"
 
 	corev1 "k8s.io/api/core/v1"
@@ -17,13 +16,13 @@ type Certificate struct {
 }
 
 // Fetch secret
-func Fetch(ctx context.Context, client client.Client, ref *corev1.SecretReference) (cert *Certificate, err error) {
+func Fetch(ctx context.Context, client client.Client, ref *corev1.SecretReference) (*Certificate, error) {
 	if ref == nil {
 		return nil, fmt.Errorf("reference is nil")
 	}
 
 	instance := &corev1.Secret{}
-	err = client.Get(
+	err := client.Get(
 		ctx,
 		types.NamespacedName{
 			Name:      ref.Name,
@@ -35,33 +34,8 @@ func Fetch(ctx context.Context, client client.Client, ref *corev1.SecretReferenc
 		return nil, err
 	}
 
-	cert = &Certificate{}
-
-	cert.Username, err = decode(instance, "username")
-	if err != nil {
-		return nil, err
-	}
-
-	cert.Password, err = decode(instance, "password")
-	if err != nil {
-		return nil, err
-	}
-
-	return cert, nil
-}
-
-// decode key's value and decode it by base64
-func decode(secret *corev1.Secret, key string) (string, error) {
-	if secret == nil {
-		return "", fmt.Errorf("secret is nil")
-	}
-
-	// Decode to byte
-	bytes := make([]byte, base64.StdEncoding.DecodedLen(len(secret.Data[key])))
-	len, err := base64.StdEncoding.Decode(bytes, secret.Data[key])
-	if err != nil {
-		return "", err
-	}
-
-	return string(bytes[:len]), nil
+	return &Certificate{
+		Username: string(instance.Data["username"]),
+		Password: string(instance.Data["password"]),
+	}, nil
 }
