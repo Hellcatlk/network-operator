dc4a81c510e5a756ef3da4addf17e824d8cd66e5
Zou Yu <zouy.fnst@cn.fujitsu.com>
2021-05-17 03:59:44 -0400

    Update install_kubebuilder.sh, if kubebuilder is installed only need to export PATH
    Signed-off-by: Zou Yu <zouy.fnst@cn.fujitsu.com>

--- tools/install_kubebuilder.sh
+++ tools/install_kubebuilder.sh
@@ -5,10 +5,13 @@ set -u
 os=$(go env GOOS)
 arch=$(go env GOARCH)
 
-# download kubebuilder and extract it to tmp
-curl -L https://go.kubebuilder.io/dl/2.3.1/"${os}"/"${arch}" | tar -xz -C /tmp/
+if [ ! -d "/usr/local/kubebuilder" ]; then
+    # download kubebuilder and extract it to tmp
+    curl -L https://go.kubebuilder.io/dl/2.3.1/"${os}"/"${arch}" | tar -xz -C /tmp/
 
-# move to a long-term location and put it on your path
-# (you'll need to set the KUBEBUILDER_ASSETS env var if you put it somewhere else)
-sudo mv /tmp/kubebuilder_2.3.1_"${os}"_"${arch}" /usr/local/kubebuilder
+    # move to a long-term location and put it on your path
+    # (you'll need to set the KUBEBUILDER_ASSETS env var if you put it somewhere else)
+    sudo mv /tmp/kubebuilder_2.3.1_"${os}"_"${arch}" /usr/local/kubebuilder
+fi
+
 export PATH=$PATH:/usr/local/kubebuilder/bin
