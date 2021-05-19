d17b8ee2050e49e03d786f3f4d074fbf86f28399
Zou Yu <zouy.fnst@cn.fujitsu.com>
2021-05-19 04:28:09 -0400

    Fix CI failed
    Signed-off-by: Zou Yu <zouy.fnst@cn.fujitsu.com>

--- hack/integration.sh
+++ hack/integration.sh
@@ -75,7 +75,6 @@ spec:
 # Wait for controller up
 echo "Wait for controller up..."
 while kubectl get deployment network-operator-controller-manager -n network-operator-system | grep -w "0/1" >/dev/null; do
-  kubectl get all -n network-operator-system
   sleep 10
 done
 
