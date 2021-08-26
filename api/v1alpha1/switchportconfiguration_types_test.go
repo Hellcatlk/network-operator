56bde2a5e2db70a3d250f398f2e1a705af90c221
Zou Yu <zouy.fnst@cn.fujitsu.com>
2021-08-26 05:41:53 -0400

    Add unit test
    Signed-off-by: Zou Yu <zouy.fnst@cn.fujitsu.com>

---
+++ api/v1alpha1/switchportconfiguration_types_test.go
@@ -0,0 +1,56 @@
+package v1alpha1
+
+import "testing"
+
+func TestIsEqual(t *testing.T) {
+	cases := []struct {
+		target   *SwitchPortConfigurationSpec
+		actual   *SwitchPortConfigurationSpec
+		expected bool
+	}{
+		{
+			target:   nil,
+			actual:   nil,
+			expected: true,
+		},
+		{
+			target:   &SwitchPortConfigurationSpec{},
+			actual:   nil,
+			expected: false,
+		},
+		{
+			target:   nil,
+			actual:   &SwitchPortConfigurationSpec{},
+			expected: false,
+		},
+		{
+			target: &SwitchPortConfigurationSpec{
+				VLANs: "1-10,11,12",
+			},
+			actual: &SwitchPortConfigurationSpec{
+				VLANs: "1-12",
+			},
+			expected: true,
+		},
+		{
+			target: &SwitchPortConfigurationSpec{
+				UntaggedVLAN: new(int),
+				VLANs:        "1-10,11,12",
+			},
+			actual: &SwitchPortConfigurationSpec{
+				UntaggedVLAN: new(int),
+				VLANs:        "1-12",
+			},
+			expected: true,
+		},
+	}
+
+	for _, c := range cases {
+		t.Run("", func(t *testing.T) {
+			got := c.target.IsEqual(c.actual)
+			if c.expected != got {
+				t.Errorf("Expected: %v, got: %v", c.expected, got)
+			}
+		})
+	}
+}
