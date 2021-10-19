8193b116178922254047ea4d18d79c187902dd2a
Zhou Hao <zhouhao@fujitsu.com>
2021-10-19 16:57:08 +0800

    Add integration test
    Signed-off-by: Zhou Hao <zhouhao@fujitsu.com>

--- Dockerfile
+++ Dockerfile
@@ -6,14 +6,17 @@ COPY ./bin/manager .
 COPY ./bin/network-runner /usr/bin
 
 # Prepare running environment
-RUN apk add ansible openssh sshpass py3-pip gcc g++ --no-cache && \
+RUN apk add ansible openssh sshpass py3-pip gcc g++ --no-cache git && \
     apk add python3-dev libc-dev linux-headers --no-cache && \
-    # Install network-runner
     pip3 install networking-ansible && \
-    ln -s /usr/lib/python3.9/site-packages/etc/ansible /etc/ && \
     echo "StrictHostKeyChecking no" >> /etc/ssh/ssh_config && \
-    # Install fos ansible plugin
     ansible-galaxy collection install fujitsu.fos && \
-    ln -s ~/.ansible/collections/ansible_collections/fujitsu/fos/plugins ~/.ansible/
+    ln -s ~/.ansible/collections/ansible_collections/fujitsu/fos/plugins ~/.ansible/ && \
+    git clone https://github.com/ansible-network/network-runner.git && \
+    rm -rf /usr/lib/python3.9/site-packages/network_runner && \
+    mv -f ./network-runner/network_runner /usr/lib/python3.9/site-packages/ && \
+    cp -rf ./network-runner/etc/ansible /etc/ && \
+    rm -rf ./network-runner
+
 
 ENTRYPOINT ["/manager"]
---
+++ hack/test_integration.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+
+set -ue
+
+# install openvswitch
+sudo apt install openvswitch-switch
+# create bridge an port
+sudo ovs-vsctl add-br br-test
+sudo ovs-vsctl add-port br-test port-test
+
+# Add test user
+sudo useradd test -m -s /bin/bash
+sudo echo test:test | sudo chpasswd
+sudo usermod -aG sudo test
+sudo chmod +w /etc/sudoers
+sudo sed -i '/root\tALL=(ALL:ALL)/a\test\tALL=(ALL:ALL) ALL' /etc/sudoers
+sudo sed -i 's/%sudo\tALL=(ALL:ALL) ALL/%sudo\tALL=(ALL:ALL) NOPASSWD:ALL/g' /etc/sudoers
+sudo chmod -w /etc/sudoers
+
+ip=`ip addr | grep 'state UP' -m 1 -A2 | tail -n1 | awk '{print $2}' | cut -f1 -d'/'`
+
+kind load docker-image --name chart-testing --nodes chart-testing-control-plane network-operator:dev
+
+# Apply CR
+echo "
+---
+apiVersion: v1
+kind: Secret
+metadata:
+  name: switch-example-secret
+type: Opaque
+data:
+  username: dGVzdA==
+  password: dGVzdA==
+
+---
+apiVersion: metal3.io/v1alpha1
+kind: SwitchResource
+metadata:
+  name: switchresource-example
+spec:
+  vlanRange: 1-1000
+  tenantLimits:
+    \"user-1\":
+      namespace: default
+      vlanRange: 1-100
+---
+apiVersion: metal3.io/v1alpha1
+kind: AnsibleSwitch
+metadata:
+  name: ansible-example
+spec:
+  os: openvswitch
+  host: $ip
+  bridge: \"br-test\"
+  credentials:
+    name: switch-example-secret
+    namespace: default
+---
+apiVersion: metal3.io/v1alpha1
+kind: Switch
+metadata:
+  name: switch-example
+spec:
+  provider:
+    kind: AnsibleSwitch
+    name: ansible-example
+  ports:
+    \"switchport-example\":
+      name: \"port-test\"
+---
+apiVersion: metal3.io/v1alpha1
+kind: SwitchPortConfiguration
+metadata:
+  name: switchportconfiguration-example
+spec:
+  untaggedVLAN: 11
+" | kubectl apply -f -
+
+echo "Wait for controller up..."
+while kubectl get deployment network-operator-controller-manager -n network-operator-system | grep -w "0/1" >/dev/null; do
+  sleep 10s
+done
+
+echo "Verify port configuration feature"
+# Modify switchPort to trigger port configuration
+kubectl get switchport switchport-example -o yaml > switchport.yaml && sed -i "s/^spec: {}/spec:\n  configuration:\n    name: switchportconfiguration-example/g" switchport.yaml && kubectl replace -f switchport.yaml
+while [ -z "$(kubectl get switchport switchport-example -o yaml | grep "state: Active")" ]; do
+  sleep 5s
+done
+# Verify that the port configuration is successful
+if [ -z "$(sudo ovs-vsctl show | grep "tag: 11")" ]; then
+  echo "Switch port configuration error"
+  exit 1
+fi
+
+echo "Verify port deConfiguration feature"
+# Modify switchPort to trigger port configuration
+kubectl get switchport switchport-example -o yaml > switchport.yaml && sed -i '/name: switchportconfiguration-example/,+1d' switchport.yaml && kubectl replace -f switchport.yaml
+sleep 10s
+while [ -z "$(kubectl get switchport switchport-example -o yaml | grep "state: Idle")" ]; do
+  sleep 10s
+done
+# Verify that the port configuration is successful
+if [ -n "$(sudo ovs-vsctl show | grep "tag: 11")" ]; then
+  echo "Switch port deConfiguration error"
+  exit 1
+fi
